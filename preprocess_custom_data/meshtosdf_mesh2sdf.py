f4b3d0827312a83af434c13ac09b0b48be6ba475
rachmadionl <rachmadiornl@gmail.com>
2024-04-25 22:03:36 +0200

    mesh2sdf, yaml for nphm

---
+++ preprocess_custom_data/meshtosdf_mesh2sdf.py
@@ -0,0 +1,34 @@
+import os
+import sys
+import trimesh
+import mesh2sdf
+import numpy as np
+import time
+
+
+filename = '/home/lazuardi/NeuralHaircutNPHM/implicit-hair-data/data/nphm/039/39_mesh.obj'
+
+mesh_scale = 0.8
+size = 256
+level = 2 / size
+
+mesh = trimesh.load(filename, force='mesh')
+
+# normalize mesh
+vertices = mesh.vertices
+bbmin = vertices.min(0)
+bbmax = vertices.max(0)
+center = (bbmin + bbmax) * 0.5
+scale = 2.0 * mesh_scale / (bbmax - bbmin).max()
+vertices = (vertices - center) * scale
+
+# fix mesh
+print(f'Computing SDF with resolution {size}...')
+t0 = time.time()
+sdf, mesh = mesh2sdf.compute(
+    vertices, mesh.faces, size, fix=True, level=level, return_mesh=True)
+t1 = time.time()
+
+# output
+mesh.vertices = mesh.vertices / scale + center
+mesh.export('mesh_256.obj')
